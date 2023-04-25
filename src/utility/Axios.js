12045ba5f202c9694d3605dd8fd5120b290ab82b
BBSharps <billybriarsharps@gmail.com>
2023-04-25 19:19:32 +0100

    add id, filter working and google maps link opens on new page

--- src/utility/Axios.js
+++ src/utility/Axios.js
@@ -5,7 +5,19 @@ const api = axios.create({
 });
 
 export const getCandidates = () => {
-  return api.get(`candidates`).then((response) => {
+  return api.get(`/candidates`).then((response) => {
     return response.data.candidates;
+  });
+};
+export const getExams = (id, location, date, order) => {
+  let request = `/exams?id=${id}`;
+  if (location !== undefined) {
+    request += `&location=${location}`;
+  }
+  date !== undefined
+    ? (request += `&date=${date}`)
+    : (request += `&order=${order}`);
+  return api.get(request).then((response) => {
+    return response.data.exams;
   });
 };
