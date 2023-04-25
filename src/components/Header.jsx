4bbd341b86922d7a11f6864b3c3ca6101cdd8a4d
BBSharps <billybriarsharps@gmail.com>
2023-04-25 12:07:04 +0100

    removed routes to avoid refresh losing data, add home and login, add axios for api calls

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import "./Header.css";
 
-function Header({ setCandidate, candidate }) {
+function Header({ candidate }) {
   return (
     <header id="Header">
-      <h1 className="Header_h1">My Exam Day</h1>
+      <h1 className="Header_h1">Exam Day</h1>
       <div className="Header_div">
         <button
           hidden={candidate}
           className="Header_button"
           onClick={(e) => {
-            setCandidate(!candidate);
+            window.location.reload();
           }}
         >
           log out
